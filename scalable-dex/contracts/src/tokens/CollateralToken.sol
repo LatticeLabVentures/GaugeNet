90fdb6e97e7a4de85a5f3b11c5427199f9b76b7b
Gokul Alex <gokulsan@users.noreply.github.com>
2021-02-06 23:55:19 +0530

    Create CollateralToken.sol

---
+++ scalable-dex/contracts/src/tokens/CollateralToken.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.5.2;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+
+contract CollateralToken is ERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint256 initialSupply,
+        uint8 tokenDecimals
+    ) public {
+        name = tokenName;
+        symbol = tokenSymbol;
+        decimals = tokenDecimals;
+        _mint(msg.sender, initialSupply * 10**uint256(decimals));
+    }
+}
